6c972bb364586be00a177a72c37c5fa15570cae3
Johnathan Ciqueira <jciqueira@annex>
2018-10-08 17:22:14 -0300

    first commit

---
+++ financas/src/br/com/caelum/financas/teste/TesteMovimentacoesComCategoria.java
@@ -0,0 +1,62 @@
+package br.com.caelum.financas.teste;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Categoria;
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteMovimentacoesComCategoria {
+
+	public static void main(String[] args) {
+		
+		Categoria categoria1 = new Categoria("viagem");
+		Categoria categoria2 = new Categoria("negocios ");
+		
+		Conta conta = new Conta();
+		conta.setId(2);
+		
+		Movimentacao movimentacao1 = new Movimentacao();
+		movimentacao1.setData(Calendar.getInstance());  //hoje
+		movimentacao1.setDescricao("Viagem à SP");
+		movimentacao1.setTipo(TipoMovimentacao.SAIDA);
+		movimentacao1.setValor(new BigDecimal("100.0"));
+		movimentacao1.setCategorias(Arrays.asList(categoria1, categoria2));
+		
+		movimentacao1.setConta(conta);
+		
+		Movimentacao movimentacao2 = new Movimentacao();
+		movimentacao2.setData(Calendar.getInstance());  //hoje
+		movimentacao2.setDescricao("Viagem à RJ");
+		movimentacao2.setTipo(TipoMovimentacao.SAIDA);
+		movimentacao2.setValor(new BigDecimal("300.0"));
+		movimentacao2.setCategorias(Arrays.asList(categoria1, categoria2));
+
+		movimentacao2.setConta(conta);
+		
+		EntityManager em = new JPAUtil().geEntityManager();
+		
+		em.getTransaction().begin();
+		
+		em.persist(categoria1);
+		em.persist(categoria2);
+		
+		em.persist(movimentacao1);
+		em.persist(movimentacao2); 
+		
+		
+		/*Movimentacao movimentacao = em.find(Movimentacao.class, 3);
+		movimentacao.setConta(conta);
+		*/
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
